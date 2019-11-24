c9c602b4101ad05043a6f1184d8bbca7e21d700e
Mark <markyb152@hotmail.com>
2019-11-25 09:51:27 +1000

    Section 8

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
 import NavBar from '../../features/nav/navBar/NavBar';
 import { Container } from 'semantic-ui-react';
-import { Route } from 'react-router';
+import { Route, Switch, withRouter } from 'react-router';
 import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
 import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
@@ -23,13 +23,16 @@ class App extends Component {
             <Fragment>
               <NavBar />
               <Container className="main">
-                <Route path='/events' component={EventDashboard} />
+                <Switch key={this.props.location.key}>
+                <Route exact path='/events' component={EventDashboard} />
                 <Route path='/events/:id' component={EventDetailedPage} />
                 <Route path='/people' component={PeopleDashboard} />
                 <Route path='/profile/:id' component={UserDetailedPage} />
                 <Route path='/settings' component={SettingsDashboard} />
-                <Route path='/createEvent' component={EventForm} />
+                <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
                 <Route path='/test' component={TestComponent} />
+                </Switch>
+                
               </Container>
             </Fragment>
           )}
@@ -39,4 +42,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './app/layout/App';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore();
 
-console.log(store.getState());
-
-const rootEL = document.getElementById('root');
+const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>,
-        rootEL
-    );
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
 };
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    });
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
 
 render();
