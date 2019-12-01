f973316dfce314545154d58e37adc2a8db31c3f7
Mark <markyb152@hotmail.com>
2019-12-01 13:26:09 +1000

    Section 16

--- src/index.js
+++ src/index.js
@@ -40,11 +40,11 @@ if (module.hot) {
   });
 }
 
-//store.firebaseAuthIsReady.then(() => {
-  
-//})
+store.firebaseAuthIsReady.then(() => {
+  render();
+})
 
-render();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
